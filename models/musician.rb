62d7cd66a9fec84aca876473080bf2ee353ac0b5
Adam Nattrass <nattrassadam@gmail.com>
2016-11-26 12:27:40 +0000

    save function for musician class

--- models/musician.rb
+++ models/musician.rb
@@ -11,5 +11,19 @@ class Musician
         @musician_pic_url = options['musician_pic_url']
         @id = options['id'].to_i
         @band_id = options['band_id'].to_i
+    end
 
-     
+
+    def save()
+        sql = "INSERT INTO musicians
+        (name, instrument, days_here, musician_pic_url, band_id)
+        VALUES
+        ('#{@name}', '#{@instrument}', #{@days_here}, '#{@musician_pic_url}', #{@band_id})
+        returning * ;"
+
+        result = SqlRunner.run(sql)
+
+        @id = result[0]['id'].to_i
+    end
+
+end
