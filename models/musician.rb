95030ab6cbf97fbf46482a3aee3c645fdff5f64a
Adam Nattrass <nattrassadam@gmail.com>
2016-11-26 12:15:42 +0000

    initialize musician

--- models/musician.rb
+++ models/musician.rb
@@ -5,5 +5,11 @@ class Musician
     attr_reader :name, :instrument, :days_here, :musician_pic_url, :id
 
     def initialize( options )
-        @name = options
+        @name = options['name']
+        @instrument = options['instument']
+        @days_here = options['days_here'].to_i
+        @musician_pic_url = options['musician_pic_url']
+        @id = options['id'].to_i
+        @band_id = options['band_id'].to_i
 
+     
