c1fe83a0bdace3f585be4001a280fdbcc720f9b0
Adam Nattrass <nattrassadam@gmail.com>
2016-11-26 13:43:23 +0000

    created view files and started to make the controller

---
+++ musician_controller.rb
@@ -0,0 +1,37 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('models/musician')
+
+get '/musicians' do
+    @musicians = Musician.all()
+    erb(:index)
+end
+
+
+# get '/students' do
+#     @students = Student.all()
+#     erb(:index)
+# end
+
+# get '/students/new' do
+#     @houses = House.all()
+#     erb(:new)
+# end
+
+# post '/students' do
+#     student = Student.new( params )
+#     student.save()
+#     redirect to '/students'
+# end
+
+# get '/students/:id' do
+#     @student = Student.find_by_id( params['id'] )
+#     erb(:show) 
+# end
+
+# get '/students/house/:house_id' do
+#     @students = Student.find_by_house( params['house_id'])
+#     @house = House.find_by_id( params['house_id'] )
+#     erb(:house)
+# end
