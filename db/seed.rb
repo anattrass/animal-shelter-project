bf935658b4c6cba6a5cb0f0d286c5fc00c95003e
Adam Nattrass <nattrassadam@gmail.com>
2016-11-26 13:20:49 +0000

    all function added to seed file

--- db/seed.rb
+++ db/seed.rb
@@ -6,7 +6,17 @@ band1 = Band.new({
     'name' => 'Slipknot'
     })
 
-band1.save
+band2 = Band.new({
+    'name' => 'Guns N Roses'
+    })
+
+band3 = Band.new({
+    'name' => 'Avenged Sevenfold'
+    })
+
+band1.save()
+band2.save()
+band3.save()
 
 musician1 = Musician.new({
     'name' => 'Corey Taylor',
@@ -16,8 +26,30 @@ musician1 = Musician.new({
     'band_id' => band1.id()
     })
 
+musician2 = Musician.new({
+    'name' => 'M Shadows',
+    'instrument' => 'Vox',
+    'days_here' => 46,
+    'musician_pic_url' => 'http://orig02.deviantart.net/a6d9/f/2010/038/f/0/m_shadows_sonisphere_by_urban01_c.jpg',
+    'band_id' => band2.id()
+    })
+
+musician3 = Musician.new({
+    'name' => 'Slash',
+    'instrument' => 'Guitar',
+    'days_here' => 31,
+    'musician_pic_url' => 'http://images5.fanpop.com/image/photos/30900000/slash-slash-30982572-377-500.jpg',
+    'band_id' => band3.id()
+    })
+
 
 musician1.save()
+musician2.save()
+musician3.save()
+
+musicians = Musician.all()
+bands = Band.all()
+
 
 binding.pry
 nil
